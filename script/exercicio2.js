ebb7ed1814bdf20cc35db158c7bac06a12a454e7
Mateus bohn <mateus.bohn.tb@gmail.com>
2026-02-23 22:21:02 -0300

    att

---
+++ script/exercicio2.js
@@ -0,0 +1,9 @@
+alert("Peça o valor que a pessoa ganha por hora e a quantidade de horas trabalhadas no mês. Mostre quanto ela vai receber no final do mês.")
+
+
+const numero = Number(prompt("Qual é o valor que você ganha por hora trabalhada ? :):"))
+const outronumero  = Number(prompt("Qual sua carga horaria trabalhada por mes? :):"))
+
+
+
+alert(numero*outronumero)
---
+++ script/exercicío3.js
@@ -0,0 +1,11 @@
+
+
+const peso1= Number(prompt("qual peso da primeira pessoa ? :):"))
+const peso2 = Number(prompt("Qual peso da segunda pessoa? :):"))
+
+const peso3 = Number(prompt("Qual peso da terceira pessoa ? :):"))
+const peso4  = Number(prompt("Qual peso da quarta pessoa ? :):"))
+const peso5  = Number(prompt("Qual peso da quinta pessoa? :):"))
+
+
+alert ((peso1+peso2+peso3+peso4+peso5)/5) 
