5333abbe89ff089a67ad0cf551cf9ae85b87e9db
Mateus bohn <mateus.bohn.tb@gmail.com>
2026-02-23 21:28:54 -0300

    feat atividade

---
+++ script/script.js
@@ -0,0 +1,11 @@
+//Não usar espaço,acento, ç caractere especial
+// const nomeCompleto
+// const NomeCompleto
+// const nome_completo
+
+//Number -é uma função  que tenta conventer um valor passado para numero 
+
+
+const numero = Number(prompt("Digite um número:"))
+const outronumero  = Number(prompt("Digite outro número:"))
+alert(numero+outronumero)
